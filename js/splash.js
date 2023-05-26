206fc0d27297f3b28466ca4492d7567dd1e3b0b3
Aiden Luce <luceaiden4124@gmail.com>
2023-05-26 14:53:29 -0500

    links

---
+++ js/splash.js
@@ -0,0 +1,7 @@
+// let gitButton = document.getElementsByClassName("git");
+// // function gitRedirect(){
+// //     // window.location.href="https://github.com/AidenLuce"
+// // }
+// gitButton.onclick(()=>{
+//     window.location.href="https://github.com/AidenLuce"
+// })
